9db01f0d56b7ed4785004a32159296751437e9d4
jynadal <nadaljeanyves@hotmail.fr>
2020-05-15 13:38:04 +0200

    quelque modification

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Tropikana';
+  bgColor = 'red';
+  show = false;
+  changeStatus(){
+    this.show = !this.show;
+  }
 }
