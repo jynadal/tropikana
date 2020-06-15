5927d296b3f8eff233f7c233283eba4482910b67
jynadal <nadaljeanyves@hotmail.fr>
2020-06-15 10:17:07 +0200

    First commit on New branch MapChart

--- trop/src/App.js
+++ trop/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Cards, Chart, CountryPicker } from './components';
+import { Cards, Chart, CountryPicker, MapChart } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
 
@@ -32,12 +32,13 @@ class App extends React.Component {
         return (
             <div className={styles.container}>
                 <img className={styles.image} src={coronaImage} alt="COVID-19" />
+                
                 <Cards data= {data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
-                <Chart data={data} country={country} />                
+                <Chart data={data} country={country} /> 
+                <MapChart />               
             </div>
         )
-
     }
 }
 
